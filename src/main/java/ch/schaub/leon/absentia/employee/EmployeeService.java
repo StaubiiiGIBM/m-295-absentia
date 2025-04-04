302777e0b9f9d1587ae8765052b557cf94755957
Leon <leon.schaub@bbzbl-it.ch>
2025-04-04 15:07:44 +0200

    Add Employee Service

---
+++ src/main/java/ch/schaub/leon/absentia/employee/EmployeeService.java
@@ -0,0 +1,46 @@
+package ch.schaub.leon.absentia.employee;
+
+import ch.schaub.leon.absentia.department.Department;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+
+    public Employee getEmployee(int id) {
+        return employeeRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Employee with id " + id + " not found")
+        );
+    }
+
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+
+    public Employee addEmployee(Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+
+    public Employee updateEmployee(Employee employee, int id) {
+        Employee originalEmployee = employeeRepository.findById(id).orElseThrow(
+                () -> new RuntimeException("Employee with id " + id + " not found")
+        );
+
+        originalEmployee.setFirst_name(employee.getFirst_name());
+        originalEmployee.setLast_name(employee.getLast_name());
+        originalEmployee.setDepartment(employee.getDepartment());
+
+        return employeeRepository.save(originalEmployee);
+    }
+
+    public String deleteEmployee(int id) {
+        employeeRepository.deleteById(id);
+        return "Employee with id " + id + " deleted";
+    }
+}
