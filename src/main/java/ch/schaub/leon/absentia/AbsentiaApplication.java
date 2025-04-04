349a74af774fd212fd831364f6a53968d4f25a5b
Leon <leon.schaub@bbzbl-it.ch>
2025-04-04 11:42:03 +0200

    Spring boot initialize

---
+++ src/main/java/ch/schaub/leon/absentia/AbsentiaApplication.java
@@ -0,0 +1,13 @@
+package ch.schaub.leon.absentia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsentiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbsentiaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ch/schaub/leon/absentia/AbsentiaApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.schaub.leon.absentia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbsentiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
