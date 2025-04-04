a7ec34e882483cce9b3621b8b42f8fcb7f5b9c33
Leon <leon.schaub@bbzbl-it.ch>
2025-04-04 14:30:11 +0200

    Add Department RestController

---
+++ src/main/java/ch/schaub/leon/absentia/department/DepartmentController.java
@@ -0,0 +1,44 @@
+package ch.schaub.leon.absentia.department;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @GetMapping("/department/{id}")
+    public ResponseEntity<Department> getDepartment(@PathVariable int id) {
+        Department department = departmentService.getDepartment(id);
+        return ResponseEntity.ok(department);
+    }
+
+    @GetMapping("/department")
+    public ResponseEntity<List<Department>> getAllDepartments() {
+        List<Department> departments = departmentService.getAllDepartments();
+        return ResponseEntity.ok(departments);
+    }
+
+    @PostMapping("/department/add")
+    public ResponseEntity<Department> addDepartment(@RequestBody Department department) {
+        Department newDepartment = departmentService.addDepartment(department);
+        return ResponseEntity.ok(newDepartment);
+    }
+
+    @PutMapping("/department/edit/{id}")
+    public ResponseEntity<Department> editDepartment(
+            @RequestBody Department department, @PathVariable int id
+    ) {
+        return ResponseEntity.ok(departmentService.updateDepartment(department, id));
+    }
+
+    @DeleteMapping("/department/delete/{id}")
+    public String deleteDepartment(@PathVariable int id) {
+        return departmentService.deleteDepartment(id);
+    }
+}
