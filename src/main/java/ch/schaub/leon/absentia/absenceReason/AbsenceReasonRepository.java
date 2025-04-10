0862b64152e329fb2440ef51c30183bdd8baf1b3
Leon <leon.schaub@bbzbl-it.ch>
2025-04-10 08:43:38 +0200

    Add missing repository annotations

--- src/main/java/ch/schaub/leon/absentia/absenceReason/AbsenceReasonRepository.java
+++ src/main/java/ch/schaub/leon/absentia/absenceReason/AbsenceReasonRepository.java
@@ -1,7 +1,9 @@
 package ch.schaub.leon.absentia.absenceReason;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AbsenceReasonRepository extends JpaRepository<AbsenceReason, Integer> {
 
 }
--- src/main/java/ch/schaub/leon/absentia/employee/EmployeeRepository.java
+++ src/main/java/ch/schaub/leon/absentia/employee/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package ch.schaub.leon.absentia.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
